d105608ea6288db1f4e2e3bba0e77c9efc6700bd
pallehpetersen <44908184+pallehpetersen@users.noreply.github.com>
2026-03-26 17:25:53 +0100

    cleanup and todos

--- src/adiar/bvec.h
+++ src/adiar/bvec.h
@@ -10,53 +10,54 @@
 namespace adiar {
     class bvec
     {
-    private:
+        public: static constexpr size_t MAX_BITLEN = 128;
+        private:
         std::vector<bdd> _bits;
-        size_t _max_length;
+        size_t _bitlen;
         const bdd default_value = bdd();
 
-    public:
+        public:
         /////////////////////////////////////////////////////////////////////////
         /// \brief Zero-length bvec constructor, effectively a "safe nullpointer"
         /////////////////////////////////////////////////////////////////////////
-        bvec(size_t max_length = SIZE_T_MAX)
-            : _bits(0), _max_length(max_length)
+        bvec(size_t bitlen = MAX_BITLEN)
+            : _bits(0), _bitlen(bitlen)
         {}
         /////////////////////////////////////////////////////////////////////////
         /// \brief Copy constructor
         /////////////////////////////////////////////////////////////////////////
         bvec(const bvec& fs) 
-            : _bits(fs._bits), _max_length(fs._max_length)
+            : _bits(fs._bits), _bitlen(fs._bitlen)
         {}
         /////////////////////////////////////////////////////////////////////////
         /// \brief Move constructor for right-hand values
         /////////////////////////////////////////////////////////////////////////
         bvec(bvec&& fs) 
-        : _bits(std::move(fs._bits)), _max_length(fs._max_length)
+        : _bits(std::move(fs._bits)), _bitlen(fs._bitlen)
         {}
 
         /////////////////////////////////////////////////////////////////////////
         /// \brief Conversion constructor from a raw bit-vector
         /////////////////////////////////////////////////////////////////////////
-        bvec(const std::vector<bdd>& bits, size_t max_length = SIZE_T_MAX) 
-        : _bits(bits), _max_length(max_length)
+        bvec(const std::vector<bdd>& bits, size_t bitlen = MAX_BITLEN) 
+        : _bits(bits), _bitlen(bitlen)
         {}
         /////////////////////////////////////////////////////////////////////////
         /// \brief Conversion constructor from a raw bit-vector for right-hand values
         /////////////////////////////////////////////////////////////////////////
-        bvec(std::vector<bdd>&& bits, size_t max_length = SIZE_T_MAX)
-        : _bits(std::move(bits)), _max_length(max_length)
+        bvec(std::vector<bdd>&& bits, size_t bitlen = MAX_BITLEN)
+        : _bits(std::move(bits)), _bitlen(bitlen)
         {}
 
         /////////////////////////////////////////////////////////////////////////
         /// \brief Parameterized constructor with length `bitlen` and given initial value `f`
         /////////////////////////////////////////////////////////////////////////
         bvec(const size_t bitlen, const bdd& f) 
-            : _bits(bitlen,f), _max_length(bitlen)
+            : _bits(bitlen,f), _bitlen(bitlen)
         {}
         
         public:
-        size_t bitlen() const { return _max_length; }
+        size_t bitlen() const { return _bitlen; }
         size_t size() const { return _bits.size(); }
 
         const bdd&              // return type
@@ -118,11 +119,11 @@ namespace adiar {
 
     // Constructors
 
-    bvec bvec_false(size_t bitlen = SIZE_T_MAX) {
+    bvec bvec_false(size_t bitlen = bvec::MAX_BITLEN) {
         return bvec(std::vector<bdd>(0,bdd_false()),bitlen);
     }
 
-    bvec bvec_true(size_t bitlen = SIZE_T_MAX) {
+    bvec bvec_true(size_t bitlen = bvec::MAX_BITLEN) {
         return bvec(std::vector<bdd>(bitlen,bdd_true()),bitlen);
     }
 
@@ -147,8 +148,29 @@ namespace adiar {
     }
 
 
+    
+    //Comparators
+
+    bool
+    bvec_equal(const bvec& x, const bvec& y) {
+        if(x.size() != y.size()) { return false; }
+        
+        for (size_t i = 0; i < x.size(); i++) {
+            if(!bdd_equal(x.at(i),y.at(i))) { return false; }
+        }
+        return true;
+    }
+
+    bool
+    operator==(const bvec& x, const bvec& y) {
+        return bvec_equal(x,y);
+    }
+
+
+    
+    // Bitwise operations
+
     //Helper function for bitwise operations
-    //TODO: Figure out how to do higher order function templating with constrains on function signature
     template<typename BDD_OP>
     bvec _bvec_bitwise_op(size_t size, size_t bitlen, const BDD_OP& op) {
         std::vector<bdd> res;
@@ -174,24 +196,6 @@ namespace adiar {
         return _bvec_bitwise_op(size,bitlen,op);
     }
 
-    //Comparators
-
-    bool
-    bvec_equal(const bvec& x, const bvec& y) {
-        if(x.size() != y.size()) { return false; }
-        
-        for (size_t i = 0; i < x.size(); i++) {
-            if(!bdd_equal(x.at(i),y.at(i))) { return false; }
-        }
-        return true;
-    }
-
-    bool
-    operator==(const bvec& x, const bvec& y) {
-        return bvec_equal(x,y);
-    }
-
-    // Boolean operations
     bvec bvec_and(bvec x, bvec y) {
         return _bvec_bitwise_op(x,y,[&](size_t i){ return bdd_and(x.at(i),y.at(i)); });
     }
@@ -207,8 +211,19 @@ namespace adiar {
     bvec bvec_not(bvec x) {
         return _bvec_bitwise_op(x.bitlen(),x.bitlen(),[&](size_t i){ return bdd_not(x.at(i)); });
     }
+
+    //Arithmetic operations
+
+    bvec 
+    bvec_plus(bvec x, bvec y) {}
+
 }
-
-
+//TODO: Move implementation to bvec.cpp
+//TODO: Use "const bvec&" instead of "bvec" (call-by-reference instead of call-by-value)
+//TODO: Rebase from main, then use (if(f) instead of if(!bdd_isfalse(f)))
+//TODO: Test overview both existing and missing, test bvec_equal, use bvec_equal instead of bitwise assertions
+//TODO: bvec_truncate(new_bitlen); use _bvec_bitwise_op(...new_bitlen...) to truncate
+//TODO: Implement arithmetic operations
+//TODO: Add variables, bvec_var(ITER begin, ITER end, bitlen)
 
 #endif // ADIAR_BVEC_H 
